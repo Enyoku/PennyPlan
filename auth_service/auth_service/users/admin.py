6986e2e00132683365ef004d14ead19613857ab5
Enyoku <mondex.2003@gmail.com>
2025-04-02 00:53:00 +0300

    Changed Base User admin to CutomUserAdmin class

--- auth_service/auth_service/users/admin.py
+++ auth_service/auth_service/users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ("username", "email", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active")
+    search_fields = ("username", "email")
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
