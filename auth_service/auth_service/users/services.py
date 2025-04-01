3cfd251fa3d435cbd9c4bbbcc0ebdaa0fe0490a4
Enyoku <mondex.2003@gmail.com>
2025-04-02 00:54:08 +0300

    Added User service

---
+++ auth_service/auth_service/users/services.py
@@ -0,0 +1,21 @@
+from typing import Optional, Dict, Any
+
+from django.contrib.auth.models import User
+
+from .repositories import UserRepo
+
+class UserService():
+    def __init__(self):
+        self.repo: UserRepo = UserRepo()
+
+    def authenticate(self, username: str, password: str) -> Optional[User]:
+        user: Optional[User] = self.repo.get_user_by_username(username)
+        if user and user.check_password(password):  # Теперь user — экземпляр модели
+            return user
+        return None
+    
+    def register(self, username: str, password: str) -> User:
+        return self.repo.create_user(username, password)
+    
+    def update_profile(self, user: User, **kwargs: Dict[str, Any]) -> User:
+        return self.repo.update_user(user, **kwargs)
