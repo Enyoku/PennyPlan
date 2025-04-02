7bf3491d6016ba1620c75dd15c0e74ce773dc2ec
Enyoku <mondex.2003@gmail.com>
2025-04-02 18:32:57 +0300

    Added email, username validation

--- auth_service/auth_service/users/serializers.py
+++ auth_service/auth_service/users/serializers.py
@@ -1,7 +1,32 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.core.validators import validate_email
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model: type[User] = User
         fields: list[str] = ["id", "username", "email"]
+
+    def validate_email(self, value: str) -> str:
+        if not value:
+            raise serializers.ValidationError("Email cannot be empty")
+    
+        try:
+            validate_email(value)
+        except:
+            raise serializers.ValidationError("Invorrect email format")
+        
+        if User.objects.filter(email=value).exclude(id=self.instance.id).exists():
+            raise serializers.ValidationError("This email was already taken")
+        
+        return value
+    
+    def validate_username(self, value: str) -> str:
+        if not value:
+            raise serializers.ValidationError("Username cannot be empty")
+        if len(value) < 3:
+            raise serializers.ValidationError("Username must contain at least 3 characters")
+        if User.objects.filter(username=value).exclude(id=self.instance.id).exists():
+            raise serializers.ValidationError("This username has already taken")
+        
+        return value
